e97dc7cf882602e9489de8270e7c79505072f0ff
Maks Verver <maks@verver.ch>
2022-07-24 22:53:21 +0200

    Implement random access.
    This work just on individual bytes, but that's okay because it's just a proof
    of concept. It's easy to extend this to ranges of bytes too.

--- xzra.c
+++ xzra.c
@@ -4,7 +4,7 @@
 //
 // xz --block-size=65536 --check=crc32 -0 --extreme --keep input.dat
 //
-// Note: only unpadded single-stream files are supported.
+// Currently, only single-stream files without padding at the end are supported.
 
 #include <lzma.h>
 
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 // For mmap()
 #include <fcntl.h>
@@ -24,30 +25,59 @@
 
 static const lzma_allocator *allocator = NULL;  // use malloc()/free()
 
+// Decompresses up to `output_size` bytes from the given block.
+//
+// The whole data file is described by `data` and `size`.
+//
+// The compressed block offset is `offset`.
+//
+// The output buffer is described by `output_data` and `output_size`.
+//
+// The size of the block may be greater or smaller than `output_size`. If the
+// block size is smaller, the actual number of bytes decoded is written to
+// *output_size. If the block size is greater, only the first *output_size
+// bytes are decoded. In both cases this function returns LMZA_OK (assuming no
+// other error occurred).
 static lzma_ret decompress_block(
     lzma_check check,
     const char *data, size_t size,
     size_t offset,
     char *output_data, size_t *output_size) {
-  lzma_filter filters[LZMA_FILTERS_MAX + 1] = {};
+  // Filter list. This is initialized by lzma_block_header_decode().
+  lzma_filter filters[LZMA_FILTERS_MAX + 1];
 
+  // Figure out the block header size.
   uint8_t size_byte = data[offset];
-  assert(size_byte != 0);
+  assert(size_byte != 0);  // 0 means a stream header instead
   lzma_block block = {};
   block.version = 1;
   block.header_size = lzma_block_header_size_decode(size_byte);
   block.check = check;
   block.filters = filters;
 
+  // Decode block header.
   assert(size - offset >= LZMA_BLOCK_HEADER_SIZE_MIN);
   lzma_ret ret = lzma_block_header_decode(&block, allocator, data + offset);
+  assert(size - offset >= block.header_size);
   if (ret == LZMA_OK) {
-    size_t in_pos = offset + block.header_size;
-    size_t out_pos = 0;
-    ret = lzma_block_buffer_decode(&block, allocator,
-      data, &in_pos, size,
-      output_data, &out_pos, *output_size);
-    *output_size = out_pos;
+    // Block header looks good. Try to decode.
+    lzma_stream stream = LZMA_STREAM_INIT;
+    ret = lzma_block_decoder(&stream, &block);
+    if (ret == LZMA_OK) {
+      stream.next_in = data + offset + block.header_size;
+      stream.avail_in = size;
+      stream.next_out = output_data;
+      stream.avail_out = *output_size;
+      // Actual decoding loop:
+      while (ret == LZMA_OK && stream.avail_out > 0) {
+        ret = lzma_code(&stream, LZMA_FINISH);
+      }
+      if (ret == LZMA_STREAM_END) {
+        ret = LZMA_OK;
+      }
+      *output_size = (char*) stream.next_out - output_data;
+    }
+    lzma_end(&stream);
   }
 
   for (size_t i = 0; i < LZMA_FILTERS_MAX; ++i) {
@@ -57,11 +87,16 @@ static lzma_ret decompress_block(
   return ret;
 }
 
-// `data` and `size` describe the entire memory mapped file.
+// Use the block index to seek to the blocks to be decoded.
+//
+// `offsets` and `noffset` describe the list uncompressed file offsets
+// whose bytes we want to print.
+// `check` is the checksum method from the global stream header.
+// `data` and `size` cover the entire memory mapped file.
 // `begin` to `end` (exclusive) gives the range of block data (i.e.,
-// excluding the header, index and footer).
-// 0 < begin <= end < size.
+// excluding the header, index and footer): 0 < begin <= end < size.
 static void process_with_index(
+    const int64_t *offsets, size_t noffset,
     lzma_check check,
     const char *data, size_t size,
     size_t begin, size_t end,
@@ -69,39 +104,62 @@ static void process_with_index(
   char *buffer_data = NULL;
   size_t buffer_size = 0;
 
-  lzma_index_iter iter;
-  lzma_index_iter_init(&iter, index);
-  while (!lzma_index_iter_next(&iter, LZMA_INDEX_ITER_NONEMPTY_BLOCK)) {
+  size_t i = 0;
+  while (i < noffset) {
+    lzma_index_iter iter;
+    lzma_index_iter_init(&iter, index);
 
-    size_t offset = iter.block.compressed_file_offset;
+    if (lzma_index_iter_locate(&iter, offsets[i]) == true) {
+      fprintf(stderr, "Offset out of bounds: %lld!\n", i);
+      exit(1);
+      return;
+    }
 
-    assert(begin <= offset && offset <= end);
+    const int64_t offset = iter.block.uncompressed_file_offset;
+    const int64_t block_size = iter.block.uncompressed_size;
 
-    size_t output_size = iter.block.uncompressed_size;
+    // See how many offsets we can satisfy from this block.
+    assert(offsets[i] >= offset);
+    assert(offsets[i] - offset <= block_size);
+    int j = i + 1;
+    while (j < noffset && offsets[j] - offset < block_size) ++j;
 
+    // How many bytes we need to decode. This is only part of the block.
+    size_t output_size = offsets[j - 1] - offset + 1;
+
+    // Ensure we have enough buffer space.
     if (output_size > buffer_size) {
+      assert(output_size < BUFFER_MEMORY_LIMIT);
       buffer_size = output_size;
       buffer_data = realloc(buffer_data, buffer_size);
       assert(buffer_data != NULL);
     }
 
-    // Note: the current code just decompresses and prints all
-    // block data. For random-access, we should skeep unwanted blocks, or
-    // even better, use lzma_index_iter_locate() to find the correct block
-    // directly.
-
+    const int64_t coffset = iter.block.compressed_file_offset;
+    assert(begin <= coffset && coffset <= end);
+    size_t decoded_output_size = output_size;
     lzma_ret ret = decompress_block(
-        check, data, size, offset, buffer_data, &output_size);
+        check, data, size, coffset, buffer_data, &decoded_output_size);
     assert(ret == LZMA_OK);
-    assert(output_size == iter.block.uncompressed_size);
+    assert(decoded_output_size == output_size);
 
-    fwrite(buffer_data, 1, output_size, stdout);
+    // Output the requested bytes only.
+    while (i < j) {
+      char byte = buffer_data[offsets[i] - offset];
+      fputc(byte, stdout);
+      ++i;
+    }
   }
-
   free(buffer_data);
 }
 
-static void process(const char *data, size_t size) {
+// Reads `noffset` bytes at the given uncompressed file offsets from a
+// compressed .xz file (mapped into memory at a location described by `data` and
+// `size) and prints them to standard output. Offsets MUST be given in
+// nondecreasing order.
+static void process(
+    const int64_t *offsets, size_t noffset,
+    const char *data, size_t size) {
   assert(size >= 2*LZMA_STREAM_HEADER_SIZE);
 
   lzma_stream_flags header_flags = {};
@@ -126,9 +184,8 @@ static void process(const char *data, size_t size) {
   ret = lzma_index_buffer_decode(&index, &memlimit, allocator, data, &in_pos, size);
   assert(ret == LZMA_OK && index != NULL);
 
-  fprintf(stderr, "Index uses %llu bytes of memory.\n", lzma_index_memused(index));
-
-  process_with_index(header_flags.check, data, size,
+  process_with_index(
+      offsets, noffset, header_flags.check, data, size,
       LZMA_STREAM_HEADER_SIZE, index_start_pos, index);
 
   lzma_index_end(index, allocator);
@@ -136,8 +193,21 @@ static void process(const char *data, size_t size) {
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
-    fprintf(stderr, "Usage: xzra <input.xz>\n");
+    fprintf(stderr, "Usage: xzra <input.xz> [<byte-offset>...]\n");
     exit(1);
+  }
+
+  size_t noffset = argc - 2;
+  int64_t *offsets = calloc(noffset, sizeof(*offsets));
+  for (int i = 0; i < noffset; ++i) {
+    if (sscanf(argv[i + 2], "%lld", &offsets[i]) != 1) {
+      fprintf(stderr, "Failed to parse offset %d: %s\n", i + 1, argv[i + 2]);
+      exit(1);
+    }
+    if (i > 0 && offsets[i] < offsets[i - 1]) {
+      fprintf(stderr, "Offsets must be nondecreasing!\n");
+      exit(1);
+    }
   }
 
   int fd = open(argv[1], O_RDONLY);
@@ -160,8 +230,9 @@ int main(int argc, char *argv[]) {
   }
   close(fd);
 
-  process(data, size);
+  process(offsets, noffset, data, size);
 
   munmap(data, size);
+  free(offsets);
   return 0;
 }
